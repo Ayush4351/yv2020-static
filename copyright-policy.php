e432b909f2ce66d4686b6483114223b56da3227e
Ayush4351 <ayush4351@gmail.com>
2020-04-23 23:27:37 +0530

    Updated LICENSE and Copyright Policy

--- copyright-policy.php
+++ copyright-policy.php
@@ -40,11 +40,42 @@
                 <div class="about-lpu" style="text-align: justify;">
                     <h1 style="font-size: 3em;">Copyright Policy</h1>
                     <p class="lead">
-                        All files and information contained in this Website are copyright by YouthVibe - LPU's Global Open Fest, and may not be duplicated, copied, modified or adapted, in any way without our written permission. Our Website may contain our service marks or trademarks as well as those of our affiliates or other companies, in the form of words, graphics, and logos. Your use of our Website, or Services does not constitute any right or license for you to use our service marks or trademarks, without the prior written permission of Lovely Professional University. Our Content, as found within our Website, Services, is protected under Indian and foreign copyrights. The copying, redistribution, use or publication by you of any such Content, is strictly prohibited. Your use of our Website and Services does not grant you any ownership rights to our Content.
+                        Copyright &copy; 2020 Lovely Professional University (http://youthvibe.lpu.in) All rights reserved.
                     </p>
-                    <blockquote class="blockquote">
-                        <p class="mb-0">Copyright &copy; YouthVibe - LPU's Global Open Fest. All Rights Reserved.</p>
-                    </blockquote>
+                    <p class="lead">
+                        Redistribution and use in source and binary forms, with or without modification,
+                        are permitted provided that the following conditions are met:
+                    </p>
+                    <p class="lead">
+                        <ul>
+                            <li class="lead">
+                                Redistributions of source code must retain the above copyright notice, this
+                                list of conditions and the following disclaimer.
+                            </li>
+                            <li class="lead">
+                                Redistributions in binary form must reproduce the above copyright notice, this
+                                list of conditions and the following disclaimer in the documentation and/or
+                                other materials provided with the distribution.
+                            </li>
+                            <li class="lead">
+                                Neither the name of “Youth Vibe - LPU's Global Open Fest” nor the names of its contributors may be used to
+                                endorse or promote products derived from this software without specific prior
+                                written permission.
+                            </li>
+                        </ul>
+                    </p>
+                    <p class="lead">
+                        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+                        ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+                        WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+                        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+                        ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+                        (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+                        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+                        ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+                        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+                        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+                    </p>
                 </div>
             </div>
         </div>
